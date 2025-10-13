06b2fb015bc42af5b905df9585e9aae23479b270
Krarid <jmz.krarid@outlook.com>
2025-10-13 09:45:48 -0600

    feat: integration between service endpoints and database

--- app/routers/services.py
+++ app/routers/services.py
@@ -40,16 +40,32 @@ async def get_service(db: db_dependency, service_id: int = Path(gt=0)):
     if service_model is not None:
         return service_model
 
-    raise HTTPException(status_code=404, detail='Service not found.')
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Service not found.')
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
-async def create_service(service_request: ServiceRequest):
-    pass
+async def create_service(db: db_dependency, service_request: ServiceRequest):
+    service_model = Services(**service_request.model_dump())
+    db.add(service_model)
+    db.commit()
 
 @router.put("/{service_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def update_service(service_request: ServiceRequest, service_id: int = Path(gt=0)):
-    pass
+async def update_service(db: db_dependency, service_request: ServiceRequest, service_id: int = Path(gt=0)):
+    service_model = db.query(Services).filter(Services.id == service_id).first()
+
+    if service_model is not None:
+        for key, value in service_request.model_dump().items():
+            setattr(service_model, key, value)
+        db.commit()
+        return
+
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Service not found.')
 
 @router.delete("/{service_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_service(service_id: int = Path(gt=0)):
-    pass
+async def delete_service(db: db_dependency, service_id: int = Path(gt=0)):
+    service_model = db.query(Services).filter(Services.id == service_id).first()
+
+    if service_model is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Service not found.')
+
+    db.delete(service_model)
+    db.commit()
