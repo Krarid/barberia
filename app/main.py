777f57c6988b73366933cd52ff5493654d92ff8f
Javier Meléndez <jmz.krarid@outlook.com>
2025-10-23 00:07:55 -0600

    feat: add auth at the beginning of endpoints

--- app/main.py
+++ app/main.py
@@ -4,9 +4,9 @@ from .routers import services, customers, barbers, appointments, stock, auth
 
 app = FastAPI()
 
+app.include_router(auth.router)
 app.include_router(services.router)
 app.include_router(customers.router)
 app.include_router(barbers.router)
 app.include_router(appointments.router)
 app.include_router(stock.router)
-app.include_router(auth.router)
